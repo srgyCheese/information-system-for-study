94c196caebb97d30f172788d55b0113110be53eb
srgyCheese <sergey.sirtsilin@gmail.com>
2023-01-28 21:04:58 +0400

    add middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken')
+const sequelize = require('../models/sequelize')
+
+const {User} = sequelize.models
+
+const authMiddleware = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+
+        if (!token) {
+            return res.status(403).json({message: "Пользователь не авторизован"})
+        }
+
+        const decodedData = jwt.verify(token, process.env.JWT_SECRET)
+        
+        req.user = await User.findOne({where: {id: decodedData.id}})
+
+        if (!next) {
+            return
+        }
+
+        next()
+    } catch (e) {
+        console.log(e)
+        return res.status(403).json({message: "Пользователь не авторизован"})
+    }
+}
+
+module.exports = authMiddleware
